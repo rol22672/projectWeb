f9b128adf99bb0c890e52e2d5c125887e7263b70
davisBold <142925428+davisBold@users.noreply.github.com>
2024-04-30 21:13:03 -0600

    add ssl

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://programmerscrew.com:49160/',
+  baseURL: 'https://programmerscrew.com:49162/',
 });
 
 export default api;
